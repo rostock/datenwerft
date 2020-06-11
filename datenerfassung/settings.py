fd10ec4556712a1f409e95d993d0bf5a0dfbf303
Sebastian Gutzeit <sebastian.gutzeit@rostock.de>
2020-06-11 09:25:36 +0200

    Versionsnummer

--- datenerfassung/settings.py
+++ datenerfassung/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Application definition
 
-DATENMANAGEMENT_VERSION = '2.3.3'
+DATENMANAGEMENT_VERSION = '2.5.4'
 
 DJANGO_APPS = [
   'django.contrib.admin',
