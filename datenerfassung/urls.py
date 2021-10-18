292ce5e7ec9d3054795edc72bd4aded1311ac9c0
sg656 <stephan.glauner@uni-rostock.de>
2021-10-18 09:21:52 +0200

    Konvertierung von datenerfassung/urls.py in PEP8

--- datenerfassung/urls.py
+++ datenerfassung/urls.py
@@ -24,134 +24,162 @@ from rest_framework import routers, serializers, viewsets
 
 
 class PermissionSerializer(serializers.HyperlinkedModelSerializer):
-  url = serializers.HyperlinkedIdentityField(view_name = 'permission-detail', lookup_field = 'codename')
-  content_type = serializers.HyperlinkedRelatedField(
-    view_name = 'contenttype-detail',
-    lookup_field = 'model',
-    many = False,
-    read_only = True,
-  )
-  class Meta:
-    model = Permission
+    url = serializers.HyperlinkedIdentityField(
+        view_name='permission-detail',
+        lookup_field='codename'
+    )
+    content_type = serializers.HyperlinkedRelatedField(
+        view_name='contenttype-detail',
+        lookup_field='model',
+        many=False,
+        read_only=True,
+    )
+
+    class Meta:
+        model = Permission
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
-  url = serializers.HyperlinkedIdentityField(view_name = 'user-detail')
-  user_permissions = serializers.HyperlinkedRelatedField(
-    view_name = 'permission-detail',
-    lookup_field = 'codename',
-    many = True,
-    read_only = True,
-  )
-  class Meta:
-    model = User
-    exclude = ('password',)
+    url = serializers.HyperlinkedIdentityField(view_name='user-detail')
+    user_permissions = serializers.HyperlinkedRelatedField(
+        view_name='permission-detail',
+        lookup_field='codename',
+        many=True,
+        read_only=True,
+    )
+
+    class Meta:
+        model = User
+        exclude = ('password',)
 
 
 class GroupSerializer(serializers.HyperlinkedModelSerializer):
-  permissions = serializers.HyperlinkedRelatedField(
-    view_name = 'permission-detail',
-    lookup_field = 'codename',
-    many = True,
-    read_only = True,
-  )
-  class Meta:
-    model = Group
+    permissions = serializers.HyperlinkedRelatedField(
+        view_name='permission-detail',
+        lookup_field='codename',
+        many=True,
+        read_only=True,
+    )
+
+    class Meta:
+        model = Group
 
 
 class ContentTypeSerializer(serializers.HyperlinkedModelSerializer):
-  class Meta:
-    model = ContentType
-    lookup_field = 'model'
+    class Meta:
+        model = ContentType
+        lookup_field = 'model'
 
 
 class UserObjectPermissionSerializer(serializers.HyperlinkedModelSerializer):
-  url = serializers.HyperlinkedIdentityField(view_name = 'userobjectpermission-detail')
-  content_type = serializers.HyperlinkedRelatedField(
-    view_name = 'contenttype-detail',
-    lookup_field = 'model',
-    many = False,
-    read_only = True,
-  )
-  user = serializers.HyperlinkedRelatedField(view_name = 'user-detail', many = False, read_only = True)
-  permission = serializers.HyperlinkedRelatedField(
-    view_name = 'permission-detail',
-    lookup_field = 'codename',
-    many = False,
-    read_only = True,
-  )
-  class Meta:
-    model = UserObjectPermission
+    url = serializers.HyperlinkedIdentityField(
+        view_name='userobjectpermission-detail'
+    )
+    content_type = serializers.HyperlinkedRelatedField(
+        view_name='contenttype-detail',
+        lookup_field='model',
+        many=False,
+        read_only=True,
+    )
+    user = serializers.HyperlinkedRelatedField(
+        view_name='user-detail',
+        many=False,
+        read_only=True
+    )
+    permission = serializers.HyperlinkedRelatedField(
+        view_name='permission-detail',
+        lookup_field='codename',
+        many=False,
+        read_only=True,
+    )
+
+    class Meta:
+        model = UserObjectPermission
 
 
 class GroupObjectPermissionSerializer(serializers.HyperlinkedModelSerializer):
-  url = serializers.HyperlinkedIdentityField(view_name = 'groupobjectpermission-detail')
-  content_type = serializers.HyperlinkedRelatedField(
-    view_name = 'contenttype-detail',
-    lookup_field = 'model',
-    many = False,
-    read_only = True,
-  )
-  group = serializers.HyperlinkedRelatedField(view_name = 'group-detail', many = False, read_only = True)
-  permission = serializers.HyperlinkedRelatedField(
-    view_name = 'permission-detail',
-    lookup_field = 'codename',
-    many = False,
-    read_only = True,
-  )
-  class Meta:
-    model = GroupObjectPermission
+    url = serializers.HyperlinkedIdentityField(
+        view_name='groupobjectpermission-detail'
+    )
+    content_type = serializers.HyperlinkedRelatedField(
+        view_name='contenttype-detail',
+        lookup_field='model',
+        many=False,
+        read_only=True,
+    )
+    group = serializers.HyperlinkedRelatedField(
+        view_name='group-detail',
+        many=False,
+        read_only=True
+    )
+    permission = serializers.HyperlinkedRelatedField(
+        view_name='permission-detail',
+        lookup_field='codename',
+        many=False,
+        read_only=True,
+    )
 
+    class Meta:
+        model = GroupObjectPermission
+
+
+# Definition der View-Klassen als Unterklassen von viewsets.ModelViewSet
 
 class DatenmanagementViewSet(viewsets.ModelViewSet):
-  @classmethod
-  def create_custom(cls, **kwargs):
-    class CustomViewSet(cls):
-      model = kwargs["model"]
-      queryset = kwargs["model"].objects.all()
-    return CustomViewSet
+    @classmethod
+    def create_custom(cls, **kwargs):
+        class CustomViewSet(cls):
+            model = kwargs["model"]
+            queryset = kwargs["model"].objects.all()
+        return CustomViewSet
 
-  def get_serializer_class(self):
-    if self.serializer_class is not None:
-      return self.serializer_class
-    class DatenmanagementSerializer(serializers.HyperlinkedModelSerializer):
-      class Meta:
-        model = self.model
-    self.serializer_class = DatenmanagementSerializer
-    return self.serializer_class
+    def get_serializer_class(self):
+        if self.serializer_class is not None:
+            return self.serializer_class
+
+        class DatenmanagementSerializer(serializers.HyperlinkedModelSerializer):
+            class Meta:
+                model = self.model
+        self.serializer_class = DatenmanagementSerializer
+        return self.serializer_class
 
 
 class PermissionViewSet(viewsets.ModelViewSet):
-  queryset = Permission.objects.all()
-  serializer_class = PermissionSerializer
-  lookup_field = 'codename'
+    queryset = Permission.objects.all()
+    serializer_class = PermissionSerializer
+    lookup_field = 'codename'
 
 
 class UserViewSet(viewsets.ModelViewSet):
-  queryset = User.objects.all()
-  serializer_class = UserSerializer
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 
 class GroupViewSet(viewsets.ModelViewSet):
-  queryset = Group.objects.all()
-  serializer_class = GroupSerializer
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
 
 
 class ContentTypeViewSet(viewsets.ModelViewSet):
-  queryset = ContentType.objects.all()
-  serializer_class = ContentTypeSerializer
-  lookup_field = 'model'
+    queryset = ContentType.objects.all()
+    serializer_class = ContentTypeSerializer
+    lookup_field = 'model'
 
 
 class UserObjectPermissionViewSet(viewsets.ModelViewSet):
-  queryset = UserObjectPermission.objects.all()
-  serializer_class = UserObjectPermissionSerializer
+    queryset = UserObjectPermission.objects.all()
+    serializer_class = UserObjectPermissionSerializer
 
 
 class GroupObjectPermissionViewSet(viewsets.ModelViewSet):
-  queryset = GroupObjectPermission.objects.all()
-  serializer_class = GroupObjectPermissionSerializer
+    queryset = GroupObjectPermission.objects.all()
+    serializer_class = GroupObjectPermissionSerializer
 
+
+# "REST framework adds support for automatic URL routing to Django, and provides
+# you with a simple, quick and consistent way of wiring your view logic to a
+# set of URLs."
+# https://www.django-rest-framework.org/api-guide/routers/
 
 router = routers.DefaultRouter()
 router.register(r'user', UserViewSet)
@@ -164,16 +192,32 @@ router.register(r'group_object_permission', GroupObjectPermissionViewSet)
 
 app_models = apps.get_app_config('datenmanagement').get_models()
 for model in app_models:
-  modelname = model.__name__.lower()
-  router.register(modelname, DatenmanagementViewSet.create_custom(model = model), basename = modelname)
+    modelname = model.__name__.lower()
+    router.register(
+        modelname,
+        DatenmanagementViewSet.create_custom(model=model),
+        basename=modelname
+    )
 
-
+# Routen der URLs zu Views
 urlpatterns = [
-  url(r'^$', RedirectView.as_view(url = '/datenerfassung/datenmanagement')),
-  url(r'^admin/', admin.site.urls),
-  url(r'^accounts/login/$', LoginView.as_view(template_name = '_registration/login.html', redirect_authenticated_user = True), name = 'login'),
-  url(r'^accounts/logout/$', LogoutView.as_view(template_name = '_registration/logout.html'), name = 'logout'),
-  url(r'^api/', include(router.urls)),
-  url(r'^api-auth/', include('rest_framework.urls')),
-  url(r'^datenmanagement/', include('datenmanagement.urls')),
+    url(regex=r'^$',
+        view=RedirectView.as_view(url='/datenerfassung/datenmanagement')),
+    url(regex=r'^admin/',
+        view=admin.site.urls),
+    url(regex=r'^accounts/login/$',
+        view=LoginView.as_view(
+            template_name='_registration/login.html',
+            redirect_authenticated_user=True
+        ),
+        name='login'),
+    url(regex=r'^accounts/logout/$',
+        view=LogoutView.as_view(template_name='_registration/logout.html'),
+        name='logout'),
+    url(regex=r'^api/',
+        view=include(router.urls)),
+    url(regex=r'^api-auth/',
+        view=include('rest_framework.urls')),
+    url(regex=r'^datenmanagement/',
+        view=include('datenmanagement.urls')),
 ]
