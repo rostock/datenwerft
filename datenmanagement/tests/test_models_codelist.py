6cac8a245e053a1596faf33ff3d6970e717fdf36
Sebastian Gutzeit <sebastian.gutzeit@rostock.de>
2023-01-13 13:56:11 +0100

    App Datenmanagement: Erweiterung Tests

--- datenmanagement/tests/test_models_codelist.py
+++ datenmanagement/tests/test_models_codelist.py
@@ -1,6 +1,9 @@
+from django.test import override_settings
+from django.urls import reverse
 from datenmanagement.models import Adressen, Strassen, Anbieter_Carsharing
 
 from .base import DefaultTestCase
+from .constants_vars import *
 from .functions import load_sql_schema
 
 
@@ -13,13 +16,14 @@ class AdressenTest(DefaultTestCase):
   Testklasse für Adressen
   """
 
+  model = Adressen
   INITIAL = 'Deppendorfer Str. 23a'
   UPDATED = 'Suppenkasperweg 42'
 
   @classmethod
   def setUpTestData(cls):
     load_sql_schema()
-    cls.test_object = Adressen.objects.create(adresse=cls.INITIAL)
+    cls.test_object = cls.model.objects.create(adresse=cls.INITIAL)
 
   def setUp(self):
     self.init()
@@ -27,23 +31,23 @@ class AdressenTest(DefaultTestCase):
   def test_is_metamodel(self):
     # Datenmodell als Meta-Datenmodell deklariert?
     self.assertTrue(
-      hasattr(Adressen._meta, 'metamodel')
-      and Adressen._meta.metamodel is True
+      hasattr(self.model._meta, 'metamodel')
+      and self.model._meta.metamodel is True
     )
     # Datenmodell nicht bearbeitbar?
     self.assertTrue(
-      hasattr(Adressen._meta, 'editable')
-      and Adressen._meta.editable is False
+      hasattr(self.model._meta, 'editable')
+      and self.model._meta.editable is False
     )
 
   def test_create(self):
     # genau ein Objekt erstellt?
-    self.assertEqual(Adressen.objects.all().count(), 1)
+    self.assertEqual(self.model.objects.all().count(), 1)
     # erstelltes Objekt wie erwartet erstellt?
-    test_object = Adressen.objects.get(adresse=self.INITIAL)
+    test_object = self.model.objects.get(adresse=self.INITIAL)
     self.assertEqual(test_object, self.test_object)
     # erstelltes Objekt umfasst in einem seiner Felder eine bestimmte Information?
-    self.assertEqual(Adressen.objects.filter(adresse=self.INITIAL).count(), 1)
+    self.assertEqual(self.model.objects.filter(adresse=self.INITIAL).count(), 1)
     # erstelltes Objekt umfasst ein UUID-Feld, das als Primärschlüssel deklariert ist?
     self.assertEqual(test_object.pk, test_object.uuid)
 
@@ -51,17 +55,25 @@ class AdressenTest(DefaultTestCase):
     self.test_object.adresse = self.UPDATED
     self.test_object.save()
     # existiert nach der Aktualisierung immer noch genau ein Objekt?
-    self.assertEqual(Adressen.objects.all().count(), 1)
+    self.assertEqual(self.model.objects.all().count(), 1)
     # aktualisiertes Objekt wie erwartet aktualisiert?
-    test_object = Adressen.objects.get(adresse=self.UPDATED)
+    test_object = self.model.objects.get(adresse=self.UPDATED)
     self.assertEqual(test_object, self.test_object)
     # aktualisiertes Objekt umfasst in einem seiner Felder eine bestimmte Information?
-    self.assertEqual(Adressen.objects.filter(adresse=self.UPDATED).count(), 1)
+    self.assertEqual(self.model.objects.filter(adresse=self.UPDATED).count(), 1)
 
   def test_delete(self):
     # keine Objekte mehr vorhanden?
     self.test_object.delete()
-    self.assertEqual(Adressen.objects.all().count(), 0)
+    self.assertEqual(self.model.objects.all().count(), 0)
+
+  @override_settings(AUTHENTICATION_BACKENDS=['django.contrib.auth.backends.ModelBackend'])
+  def test_view_start_success(self):
+    self.client.login(
+      username=USERNAME,
+      password=PASSWORD
+    )
+    pass
 
 
 class StrassenTest(DefaultTestCase):
@@ -69,13 +81,14 @@ class StrassenTest(DefaultTestCase):
   Testklasse für Straßen
   """
 
+  model = Strassen
   INITIAL = 'Deppendorfer Str.'
   UPDATED = 'Suppenkasperweg'
 
   @classmethod
   def setUpTestData(cls):
     load_sql_schema()
-    cls.test_object = Strassen.objects.create(strasse=cls.INITIAL)
+    cls.test_object = cls.model.objects.create(strasse=cls.INITIAL)
 
   def setUp(self):
     self.init()
@@ -83,23 +96,23 @@ class StrassenTest(DefaultTestCase):
   def test_is_metamodel(self):
     # Datenmodell als Meta-Datenmodell deklariert?
     self.assertTrue(
-      hasattr(Strassen._meta, 'metamodel')
-      and Strassen._meta.metamodel is True
+      hasattr(self.model._meta, 'metamodel')
+      and self.model._meta.metamodel is True
     )
     # Datenmodell nicht bearbeitbar?
     self.assertTrue(
-      hasattr(Strassen._meta, 'editable')
-      and Strassen._meta.editable is False
+      hasattr(self.model._meta, 'editable')
+      and self.model._meta.editable is False
     )
 
   def test_create(self):
     # genau ein Objekt erstellt?
-    self.assertEqual(Strassen.objects.all().count(), 1)
+    self.assertEqual(self.model.objects.all().count(), 1)
     # erstelltes Objekt wie erwartet erstellt?
-    test_object = Strassen.objects.get(strasse=self.INITIAL)
+    test_object = self.model.objects.get(strasse=self.INITIAL)
     self.assertEqual(test_object, self.test_object)
     # erstelltes Objekt umfasst in einem seiner Felder eine bestimmte Information?
-    self.assertEqual(Strassen.objects.filter(strasse=self.INITIAL).count(), 1)
+    self.assertEqual(self.model.objects.filter(strasse=self.INITIAL).count(), 1)
     # erstelltes Objekt umfasst ein UUID-Feld, das als Primärschlüssel deklariert ist?
     self.assertEqual(test_object.pk, test_object.uuid)
 
@@ -107,17 +120,17 @@ class StrassenTest(DefaultTestCase):
     self.test_object.strasse = self.UPDATED
     self.test_object.save()
     # existiert nach der Aktualisierung immer noch genau ein Objekt?
-    self.assertEqual(Strassen.objects.all().count(), 1)
+    self.assertEqual(self.model.objects.all().count(), 1)
     # aktualisiertes Objekt wie erwartet aktualisiert?
-    test_object = Strassen.objects.get(strasse=self.UPDATED)
+    test_object = self.model.objects.get(strasse=self.UPDATED)
     self.assertEqual(test_object, self.test_object)
     # aktualisiertes Objekt umfasst in einem seiner Felder eine bestimmte Information?
-    self.assertEqual(Strassen.objects.filter(strasse=self.UPDATED).count(), 1)
+    self.assertEqual(self.model.objects.filter(strasse=self.UPDATED).count(), 1)
 
   def test_delete(self):
     # keine Objekte mehr vorhanden?
     self.test_object.delete()
-    self.assertEqual(Strassen.objects.all().count(), 0)
+    self.assertEqual(self.model.objects.all().count(), 0)
 
 
 #
@@ -129,13 +142,14 @@ class AnbieterCarsharingTest(DefaultTestCase):
   Testklasse für Carsharing-Anbieter
   """
 
+  model = Anbieter_Carsharing
   INITIAL = 'Deppendorf GmbH & Co. KG'
   UPDATED = 'Suppenkasper AG'
 
   @classmethod
   def setUpTestData(cls):
     load_sql_schema()
-    cls.test_object = Anbieter_Carsharing.objects.create(anbieter=cls.INITIAL)
+    cls.test_object = cls.model.objects.create(anbieter=cls.INITIAL)
 
   def setUp(self):
     self.init()
@@ -143,18 +157,18 @@ class AnbieterCarsharingTest(DefaultTestCase):
   def test_is_codelist(self):
     # Datenmodell als Codeliste deklariert?
     self.assertTrue(
-      hasattr(Anbieter_Carsharing._meta, 'codelist')
-      and Anbieter_Carsharing._meta.codelist is True
+      hasattr(self.model._meta, 'codelist')
+      and self.model._meta.codelist is True
     )
 
   def test_create(self):
     # genau ein Objekt erstellt?
-    self.assertEqual(Anbieter_Carsharing.objects.all().count(), 1)
+    self.assertEqual(self.model.objects.all().count(), 1)
     # erstelltes Objekt wie erwartet erstellt?
-    test_object = Anbieter_Carsharing.objects.get(anbieter=self.INITIAL)
+    test_object = self.model.objects.get(anbieter=self.INITIAL)
     self.assertEqual(test_object, self.test_object)
     # erstelltes Objekt umfasst in einem seiner Felder eine bestimmte Information?
-    self.assertEqual(Anbieter_Carsharing.objects.filter(anbieter=self.INITIAL).count(), 1)
+    self.assertEqual(self.model.objects.filter(anbieter=self.INITIAL).count(), 1)
     # erstelltes Objekt umfasst ein UUID-Feld, das als Primärschlüssel deklariert ist?
     self.assertEqual(test_object.pk, test_object.uuid)
 
@@ -162,14 +176,14 @@ class AnbieterCarsharingTest(DefaultTestCase):
     self.test_object.anbieter = self.UPDATED
     self.test_object.save()
     # existiert nach der Aktualisierung immer noch genau ein Objekt?
-    self.assertEqual(Anbieter_Carsharing.objects.all().count(), 1)
+    self.assertEqual(self.model.objects.all().count(), 1)
     # aktualisiertes Objekt wie erwartet aktualisiert?
-    test_object = Anbieter_Carsharing.objects.get(anbieter=self.UPDATED)
+    test_object = self.model.objects.get(anbieter=self.UPDATED)
     self.assertEqual(test_object, self.test_object)
     # aktualisiertes Objekt umfasst in einem seiner Felder eine bestimmte Information?
-    self.assertEqual(Anbieter_Carsharing.objects.filter(anbieter=self.UPDATED).count(), 1)
+    self.assertEqual(self.model.objects.filter(anbieter=self.UPDATED).count(), 1)
 
   def test_delete(self):
     # keine Objekte mehr vorhanden?
     self.test_object.delete()
-    self.assertEqual(Anbieter_Carsharing.objects.all().count(), 0)
+    self.assertEqual(self.model.objects.all().count(), 0)
