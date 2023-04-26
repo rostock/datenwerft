dae6c0dd5d5f805177eb3f1c1142850bd3609f49
Sebastian Gutzeit <sebastian.gutzeit@rostock.de>
2023-04-26 09:38:33 +0200

    Cronjobs fertiggestellt

--- bemas/management/commands/deletepersons.py
+++ bemas/management/commands/deletepersons.py
@@ -1,6 +1,7 @@
-from datetime import date, timedelta
+from datetime import timedelta
 from django.conf import settings
 from django.core.management.base import BaseCommand
+from django.utils import timezone
 
 from bemas.models import Complaint, Contact, Person
 from bemas.views.functions import create_log_entry
@@ -10,20 +11,22 @@ class Command(BaseCommand):
 
   def handle(self, *args, **options):
     # get complaint status change deadline date
-    deadline_date = date.today() - timedelta(days=settings.BEMAS_STATUS_CHANGE_DEADLINE_DAYS)
+    deadline_date = timezone.now() - timedelta(days=settings.BEMAS_STATUS_CHANGE_DEADLINE_DAYS)
     # get active complaints
     # (i.e. complaints with latest status change after deadline date
     # or with status "less" than closed)
-    valid_complaints = Complaint.objects.filter(
+    act_cpls = Complaint.objects.filter(
       status_updated_at__gt=deadline_date) | Complaint.objects.filter(status__ordinal__lt=2)
     # get all persons connected to contacts
-    contact_persons_ids = Contact.objects.all().values('person')
+    con_ps_ids = Contact.objects.all().values('person')
     # get all persons connected to active complaints
-    valid_complaints_persons_ids = valid_complaints.complainers_persons.all().values('person')
+    act_cpls_ps_ids = Person.objects.none().values('id')
+    for act_cpl in act_cpls:
+      act_cpls_ps_ids = act_cpls_ps_ids | act_cpl.complainers_persons.all().values('id')
     # get persons to be deleted
     # (i.e. persons not connected to contacts and active complaints)
     persons_delete = Person.objects.exclude(
-      id__in=contact_persons_ids).exclude(id__in=valid_complaints_persons_ids)
+      id__in=con_ps_ids).exclude(id__in=act_cpls_ps_ids)
     num_deleted = 0
     for person_delete in persons_delete:
       object_pk, object_str = person_delete.pk, str(person_delete)
