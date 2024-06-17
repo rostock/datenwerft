87aecf27d08ed13b4c7f392511ec5a388e0f24ec
Stephan Glauner <stephan.glauner@rostock.de>
2024-06-17 14:15:44 +0200

    set type hints for abstract model classes

--- datenmanagement/models/base.py
+++ datenmanagement/models/base.py
@@ -13,7 +13,6 @@ class Basemodel(Model):
   """
   default abstract model class
   """
-
   uuid = UUIDField(
     primary_key=True,
     default=uuid4,
@@ -21,214 +20,203 @@ class Basemodel(Model):
   )
 
   class Meta:
-    abstract = True
-    managed = False
+    abstract: bool = True
+    managed: bool = False
 
   class BasemodelMeta:
-    # Boolean:
-    # shall this model generally be editable?
-    editable = True
-    # String:
-    # description of this model
-    description = None
-    # String:
-    # short name of this model
-    # (only relevant for models with foreign keys; if not specified, verbose name is used)
-    short_name = None
-    # Boolean:
-    # shall the geometries of this model be selectable as an additional overlay layer
-    # in the maps in all form views?
-    as_overlay = False
-    # List of strings:
-    # names of those models the geometries of which
-    # shall be selectable and activated by default as additional overlay layers
-    # in the maps of the form views of this model
-    default_overlays = None
-    # Boolean:
-    # shall the form views of this model always be rendered in mobile mode?
-    forms_in_mobile_mode = False
-    # Boolean:
-    # shall the maps of the form views of this model enable high zoom levels?
-    forms_in_high_zoom_mode = False
-    # Boolean:
-    # shall the default map of the maps of the form views of this model
-    # be an aerial image, if high zoom levels are enabled?
-    forms_in_high_zoom_mode_default_aerial = False
-    # String:
-    # name of the field of this model
-    # whose value shall appear in drop-down list map filters in map views
-    # (if not specified, first element of ``ordering`` is used)
-    naming = None
+    """
+    Attributes:
+      editable: shall this model generally be editable?
+      description: description of this model
+      short_name: short name of this model (if foreign keys are used; if not specified -> verbose name)
+      as_overlay: shall geometries be selectable as overlay layer in the map in all form views?
+      default_overlays: list of default overlay layers in the map of form view
+      forms_in_mobile_mode: shall forms for this model be in mobile mode?
+      forms_in_high_zoom_mode: shall map in form view be in high zoom mode?
+      forms_in_high_zoom_mode_default_aerial: shall map in form view be an aerial image, if high zoom mode active?
+      naming: name shown in map filters drop-down menus
+      assiciated_models:
+
+    """
+    editable: bool = True
+    description: str = None
+    short_name: str = None
+    as_overlay: bool = False
+    default_overlays: list[str] = None
+    forms_in_mobile_mode: bool = False
+    forms_in_high_zoom_mode: bool = False
+    forms_in_high_zoom_mode_default_aerial: bool = False
+    naming: str = None
     # Dictionary:
     # shall other models (as keys), each referencing this model
     # with certain foreign key fields (as values), be used to provide corresponding links
     # in the form views and in the table of the list view of this model?
-    associated_models = None
+    associated_models: dict[str, str] = None
     # List of strings:
     # names of those fields of this model
     # which shall be rendered as read-only in the form views of this model
-    readonly_fields = None
+    readonly_fields: list[str] = None
     # Dictionary:
     # names of those fields of this model (as keys)
     # to which other models (as values) are assigned,
     # whose values are to be used to fill corresponding selection lists
     # in the form views of this model
-    choices_models_for_choices_fields = None
+    choices_models_for_choices_fields: dict = None
     # String:
     # name of a group of users which shall be used to fill the contact person field selection list
     # in the form views of this model
-    group_with_users_for_choice_field = None
+    group_with_users_for_choice_field: str = None
     # List of strings:
     # names of those fields of this model
     # which shall be equipped with additional foreign key links
     # in the form views of this model
-    fields_with_foreign_key_to_linkify = None
+    fields_with_foreign_key_to_linkify: list[str] = None
     # Dictionary:
     # names of those fields of this model (as keys)
     # which shall be equipped with additional external links (as values)
     # in the form views of this model
-    catalog_link_fields = None
+    catalog_link_fields: dict = None
     # String:
     # geometry type of this model
-    geometry_type = None
+    geometry_type: str = None
     # String:
     # address search class
-    address_search_class = 'address_hro'
+    address_search_class: str = 'address_hro'
     # Boolean:
     # shall address search results be shown in their long versions?
-    address_search_long_results = False
+    address_search_long_results: bool = False
     # String:
     # address reference type of this model (i.e. address, street or district)
-    address_type = None
+    address_type: str = None
     # Boolean:
     # shall an address reference be mandatory for this model?
-    address_mandatory = False
+    address_mandatory: bool = False
     # Boolean:
     # shall thumbnails be created from uploaded photos for this model?
-    thumbs = True
+    thumbs: bool = True
     # Boolean:
     # shall it be possible to upload multiple photos for this model?
     # if true, multiple datasets are created, i.e. one for each photo.
-    multi_photos = False
+    multi_photos: bool = False
     # Boolean:
     # shall an upload field for a GeoJSON file be available in the form views of this model?
-    geojson_input = False
+    geojson_input: bool = False
     # Boolean:
     # shall an upload field for a GPX file be available in the form views of this model?
-    gpx_input = False
+    gpx_input: bool = False
     # String:
     # name of the field of this model
     # which shall be equipped with a postcode assignment function
     # in the form views of this model
-    postcode_assigner = None
+    postcode_assigner: str = None
     # Dictionary:
     # names of those fields of this model (as keys)
     # which shall appear as columns in the table of the list view of this model
     # in exactly this order, with their respective labels, i.e. column headers (as values)
-    list_fields = None
+    list_fields: dict = None
     # String:
     # name of the field appearing in ``list_fields``
     # which shall be considered as an address string
     # and thus be created from the respective model fields
-    list_field_with_address_string = None
+    list_field_with_address_string: str = None
     # String:
     # name of the field of this model
     # which shall be used as a fallback when address strings cannot be used
-    list_field_with_address_string_fallback_field = None
+    list_field_with_address_string_fallback_field: str = None
     # List of strings:
     # names of those fields of this model appearing in ``list_fields``
     # whose values are of data type date and which must therefore be treated accordingly
     # for sorting in the table of the list view of this model to work
-    list_fields_with_date = None
+    list_fields_with_date: list[str] = None
     # List of strings:
     # names of those fields of this model appearing in ``list_fields``
     # whose values are of data type datetime and which must therefore be treated accordingly
     # for sorting in the table of the list view of this model to work
-    list_fields_with_datetime = None
+    list_fields_with_datetime: list[str] = None
     # List of strings:
     # names of those fields of this model appearing in ``list_fields``
     # whose values are of Decimal type and which must therefore be treated accordingly
     # for sorting in the table of the list view of this model to work
-    list_fields_with_decimal = None
+    list_fields_with_decimal: list[str] = None
     # Dictionary:
     # names of those fields appearing in ``list_fields`` (as keys)
     # which are to be converted into names of foreign key fields (as values)
     # for the table of the list view so that they can also be found and displayed
     # in the referenced table of the corresponding list view
-    list_fields_with_foreign_key = None
+    list_fields_with_foreign_key: dict = None
     # Dictionary:
     # details of a foreign key field of this model
     # which shall appear as an additional column in the table of the list view of this model
-    list_additional_foreign_key_field = None
+    list_additional_foreign_key_field: dict = None
     # List of dictionaries:
     # properties of assignment actions
     # which shall be selectable below the table of the list view of this model
-    list_actions_assign = None
+    list_actions_assign: list[dict] = None
     # String:
     # name of that Boolean field of this model
     # whose values (only if ``True``) shall be used as a flag for highlighting
     # both in the table of the list view of this model and in the map view of this model
-    highlight_flag = None
-    # Number:
+    highlight_flag: str = None
+    # Integer:
     # limit for individual map feature loading steps
     # (i.e. maximum number of map features to be loaded in one step at once)
     # in the map view of this model
-    map_heavy_load_limit = None
+    map_heavy_load_limit: int = None
     # List of strings:
     # names of the fields of this model
     # whose values shall concatenated
     # and as such be used as the source for the map feature tooltips in the map view of this model
     # (if not specified, primary key is used)
-    map_feature_tooltip_fields = None
+    map_feature_tooltip_fields: list[str] = None
     # Boolean:
     # shall the one-click map filters defined in the map view template
     # appear in the map view of this model?
-    map_one_click_filters = False
+    map_one_click_filters: bool = False
     # List of strings:
     # names of exactly two fields of this model
     # which shall appear as a deadline map filter in the map view of this model
     # (always processed in pairs!)
-    map_deadlinefilter_fields = None
+    map_deadlinefilter_fields: list[str] = None
     # Dictionary:
     # names of those fields of this model (as keys)
     # which shall appear as interval map filters in the map view of this model
     # in exactly this order, with their respective titles (as values)
     # (always processed in pairs!)
-    map_intervalfilter_fields = None
+    map_intervalfilter_fields: dict = None
     # Dictionary:
     # names of those fields of this model (as keys)
     # which shall appear as map filters in the map view of this model
     # in exactly this order, with their respective titles (as values)
-    map_filter_fields = None
+    map_filter_fields: dict = None
     # List of strings:
     # names of those fields of this model appearing in ``map_filter_fields``
     # which shall appear as drop-down list map filters in the map view of this model
-    map_filter_fields_as_list = None
+    map_filter_fields_as_list: list[str] = None
     # List of strings:
     # names of those fields of this model appearing in ``map_filter_fields``
     # which shall appear as checkbox-set map filters in the map view of this model
     # (but these must not be the fields from ``map_filter_fields``
     # which are already multiple selection fields!)
-    map_filter_fields_as_checkbox = None
+    map_filter_fields_as_checkbox: list[str] = None
     # Boolean:
     # shall those fields of this model appearing in ``map_filter_fields``
     # which are of data type Boolean
     # appear as checkbox-set map filters in the map view of this model?
-    map_filter_boolean_fields_as_checkbox = False
+    map_filter_boolean_fields_as_checkbox: bool = False
     # Dictionary:
     # name of that field of this model (as key)
     # whose specific value (as value) shall ensure that all those objects
     # that have exactly this specific value in this field
     # do not initially appear on the map in the map view of this model
-    map_filter_hide_initial = None
+    map_filter_hide_initial: dict = None
     # List of dictionaries:
     # properties of WMS which shall be selectable as additional overlay layers
     # in the maps of the form views of this model
-    additional_wms_layers = None
+    additional_wms_layers: list[dict] = None
     # List of dictionaries:
     # properties of WFS which shall be selectable as additional overlay layers
     # in the maps of the form views of this model
-    additional_wfs_featuretypes = None
+    additional_wfs_featuretypes: list[dict] = None
 
 
 class Metamodel(Basemodel):
@@ -238,10 +226,10 @@ class Metamodel(Basemodel):
   """
 
   class Meta(Basemodel.Meta):
-    abstract = True
+    abstract: bool = True
 
   class BasemodelMeta(Basemodel.BasemodelMeta):
-    editable = False
+    editable: bool = False
 
 
 class Codelist(Basemodel):
@@ -250,7 +238,7 @@ class Codelist(Basemodel):
   """
 
   class Meta(Basemodel.Meta):
-    abstract = True
+    abstract: bool = True
 
 
 #
@@ -270,11 +258,11 @@ class Art(Codelist):
   )
 
   class Meta(Codelist.Meta):
-    abstract = True
-    ordering = ['art']
+    abstract: bool = True
+    ordering: list[str] = ['art']
 
   class BasemodelMeta(Codelist.BasemodelMeta):
-    list_fields = {
+    list_fields: dict[str, str] = {
       'art': 'Art'
     }
 
@@ -295,11 +283,11 @@ class Ausfuehrung(Codelist):
   )
 
   class Meta(Codelist.Meta):
-    abstract = True
-    ordering = ['ausfuehrung']
+    abstract: bool = True
+    ordering: list[str] = ['ausfuehrung']
 
   class BasemodelMeta(Codelist.BasemodelMeta):
-    list_fields = {
+    list_fields: dict[str, str] = {
       'ausfuehrung': 'Ausführung'
     }
 
@@ -320,11 +308,11 @@ class Befestigungsart(Codelist):
   )
 
   class Meta(Codelist.Meta):
-    abstract = True
-    ordering = ['befestigungsart']
+    abstract: bool = True
+    ordering: list[str] = ['befestigungsart']
 
   class BasemodelMeta(Codelist.BasemodelMeta):
-    list_fields = {
+    list_fields: dict[str, str] = {
       'befestigungsart': 'Befestigungsart'
     }
 
@@ -345,11 +333,11 @@ class Material(Codelist):
   )
 
   class Meta(Codelist.Meta):
-    abstract = True
-    ordering = ['material']
+    abstract: bool = True
+    ordering: list[str] = ['material']
 
   class BasemodelMeta(Codelist.BasemodelMeta):
-    list_fields = {
+    list_fields: dict[str, str] = {
       'material': 'Material'
     }
 
@@ -370,11 +358,11 @@ class Schlagwort(Codelist):
   )
 
   class Meta(Codelist.Meta):
-    abstract = True
-    ordering = ['schlagwort']
+    abstract: bool = True
+    ordering: list[str] = ['schlagwort']
 
   class BasemodelMeta(Codelist.BasemodelMeta):
-    list_fields = {
+    list_fields: dict[str, str] = {
       'schlagwort': 'Schlagwort'
     }
 
@@ -395,11 +383,11 @@ class Status(Codelist):
   )
 
   class Meta(Codelist.Meta):
-    abstract = True
-    ordering = ['status']
+    abstract: bool = True
+    ordering: list[str] = ['status']
 
   class BasemodelMeta(Codelist.BasemodelMeta):
-    list_fields = {
+    list_fields: dict[str, str] = {
       'status': 'Status'
     }
 
@@ -420,11 +408,11 @@ class Typ(Codelist):
   )
 
   class Meta(Codelist.Meta):
-    abstract = True
-    ordering = ['typ']
+    abstract: bool = True
+    ordering: list[str] = ['typ']
 
   class BasemodelMeta(Codelist.BasemodelMeta):
-    list_fields = {
+    list_fields: dict[str, str] = {
       'typ': 'Typ'
     }
 
@@ -438,7 +426,16 @@ class Typ(Codelist):
 
 class SimpleModel(Basemodel):
   """
-  abstract model class for simple data models
+  Class representing a simple model.
+
+  Inherits from the Basemodel class.
+
+  Attributes:
+    aktiv (bool): Indicates if the model is active. Default is True.
+
+  Meta:
+      Inherits the Meta class from Basemodel.
+
   """
 
   aktiv = BooleanField(
@@ -447,7 +444,7 @@ class SimpleModel(Basemodel):
   )
 
   class Meta(Basemodel.Meta):
-    abstract = True
+    abstract: bool = True
 
 
 class ComplexModel(SimpleModel):
@@ -456,4 +453,4 @@ class ComplexModel(SimpleModel):
   """
 
   class Meta(SimpleModel.Meta):
-    abstract = True
+    abstract: bool = True
