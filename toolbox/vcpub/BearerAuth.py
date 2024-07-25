5b86cbf6e4cd35cd39b1fb9ab9a2b7bc69df99eb
Stephan Glauner <stephan.glauner@rostock.de>
2024-07-25 10:10:43 +0200

    Add BearerAuth extension for requests

---
+++ toolbox/vcpub/BearerAuth.py
@@ -0,0 +1,13 @@
+import requests
+
+
+class BearerAuth(requests.auth.AuthBase):
+  """
+  Extends the requests library with the option to authenticate with a bearer token at a Rest API.
+  """
+  def __init__(self, token):
+    self.token = token
+
+  def __call__(self, r):
+    r.headers["authorization"] = "Bearer " + self.token
+    return r
