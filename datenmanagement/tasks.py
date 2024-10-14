87c6d36b9b3945fa6401205ccf63b798aaa71081
Stephan Glauner <stephan.glauner@rostock.de>
2024-10-14 09:07:30 +0200

    update loggers and dependencies

--- datenmanagement/tasks.py
+++ datenmanagement/tasks.py
@@ -1,11 +1,9 @@
 # third-party imports
-from __future__ import absolute_import, unicode_literals
 from uuid import UUID
 from celery import shared_task
 
 # project imports
 from django.apps import apps
-from datenmanagement.models import Punktwolken, Punktwolken_Projekte
 from datenwerft.celery import logger
 from toolbox.vcpub.DataBucket import DataBucket
 
@@ -26,7 +24,7 @@ def send_pointcloud_to_vcpub(pk, dataset: UUID, path: str, filename: str):
       file = {filename: f}
     ok, key = bucket.upload(file=file)
     if ok:
-      logger.debug('Pointcloud uploaded to VCPub API.')
+      logger.debug('Pointcloud upload to VCPub was successful.')
       change_attr = {
         'vcp_object_key': key
       }
@@ -36,6 +34,9 @@ def send_pointcloud_to_vcpub(pk, dataset: UUID, path: str, filename: str):
         attributes=change_attr
       )
       # Todo: delete locale pointcloud file
+    else:
+      logger.error('Pointcloud upload to VCPub failed. Try again later.')
+
 
 
 @shared_task
