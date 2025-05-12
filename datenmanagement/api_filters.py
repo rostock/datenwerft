c48f17e4d8b3b033f89de514e182e1d48adfaa43
Stephan Glauner <stephan.glauner@rostock.de>
2025-05-12 13:32:38 +0200

    Add PunktwolkenFilter and proxy settings integration (#91)
    * add PunktwolkenFilter and integrate django-filter into the project
    
    * add proxy settings section to secrets.template
    
    * remove unused imports and filterset for Angelverbotsbereiche in DatenmanagementViewSet
    
    * remove unused import of Angelverbotsbereiche from api_filters.py
    
    * minor adjustment to secrets.template
    
    ---------
    
    Co-authored-by: Sebastian Gutzeit <sebastian.gutzeit@rostock.de>

---
+++ datenmanagement/api_filters.py
@@ -0,0 +1,13 @@
+import django_filters
+from .models import Punktwolken, Punktwolken_Projekte
+
+class PunktwolkenFilter(django_filters.FilterSet):
+  projekt = django_filters.ModelChoiceFilter(
+    queryset=Punktwolken_Projekte.objects.all(),
+    field_name='projekt',
+    to_field_name='uuid'  # Oder der entsprechende Feldname in Punktwolken_Projekte
+  )
+
+  class Meta:
+    model = Punktwolken
+    fields = ['projekt']
--- datenmanagement/views/api.py
+++ datenmanagement/views/api.py
@@ -1,21 +1,31 @@
 from rest_framework.serializers import HyperlinkedModelSerializer
 from rest_framework.viewsets import ModelViewSet
+from ..models import Punktwolken
+from ..api_filters import PunktwolkenFilter
 
 
 class DatenmanagementViewSet(ModelViewSet):
   model = None
+  filterset_class = None  # Add filterset_class attribute
 
   @classmethod
   def create_custom(cls, **kwargs):
+    _model = kwargs['model']
+    # Initialisiere filterset_class hier, um Scope-Probleme zu vermeiden
+    _filterset_class = None
+    if _model == Punktwolken:
+      _filterset_class = PunktwolkenFilter
+
     class CustomViewSet(cls):
-      model = kwargs['model']
-      queryset = kwargs['model'].objects.all()
+      model = _model
+      queryset = _model.objects.all()
+      filterset_class = _filterset_class # Weise die initialisierte Variable zu
 
     return CustomViewSet
 
   def get_serializer_class(self):
     if self.serializer_class is not None:
-      return self.serializer_class
+        return self.serializer_class
 
     class DatenmanagementSerializer(HyperlinkedModelSerializer):
       class Meta:
