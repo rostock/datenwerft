7fd4a42976bdca73ad9234fb7d317fbc80f700f8
elizavetashcherbakova <elizaveta@brayn.io>
2025-06-12 14:08:56 +0200

    DH-19

--- datenmanagement/models/constants_vars.py
+++ datenmanagement/models/constants_vars.py
@@ -195,8 +195,8 @@ DOKUMENT_STATUS = (
 )
 
 GUI_ELEMENTE = (
-    ('select', 'Select'),
-    ('input_text', 'Input-Text'),
-    ('input_zahl', 'Input-Zahl'),
-    ('checkbox', 'Checkbox'),
+  ('select', 'Select'),
+  ('input_text', 'Input-Text'),
+  ('input_zahl', 'Input-Zahl'),
+  ('checkbox', 'Checkbox'),
 )
