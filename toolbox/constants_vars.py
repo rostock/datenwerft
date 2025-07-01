709f20b767448fd214e47493986802a7e6d32ccd
Sebastian Gutzeit <sebastian.gutzeit@rostock.de>
2025-07-01 10:03:31 +0200

    chore: fixed regex validations

--- toolbox/constants_vars.py
+++ toolbox/constants_vars.py
@@ -49,10 +49,10 @@ d3_message = (
   'Der <strong><em>d.3</em></strong>-Vorgang muss folgendes Format aufweisen '
   '(Beispiele): 512.431-003/002 oder 002.13 oder 114.521-025'
 )
-dateiname_regex = r'^[a-z-]*(\.?[a-z]*?)$'
+dateiname_regex = r'^[0-9a-z-_]*(\.?[a-z]*?)$'
 dateiname_message = (
-  'Es dürfen nur Kleinbuchstaben – exklusive ä, ö, ü sowie ß – und Bindestriche verwendet werden'
-  'sowie optional am Ende eine durch Punkt abgetrennte Dateiendung.'
+  'Es dürfen nur Ziffern, Kleinbuchstaben – exklusive ä, ö, ü sowie ß –, Bindestriche und '
+  'Unterstriche verwendet werden sowie optional am Ende eine durch Punkt abgetrennte Dateiendung.'
 )
 doppelleerzeichen_regex = r'^(?!.*  ).*$'
 doppelleerzeichen_message = 'Texte dürfen keine doppelten Leerzeichen enthalten.'
@@ -78,9 +78,10 @@ inventarnummer_regex = r'^[0-9]{8}$'
 inventarnummer_message = (
   'Die <strong><em>Inventarnummer</em></strong> muss aus genau acht Ziffern bestehen.'
 )
-kleinbuchstaben_bindestrich_regex = r'^[a-z-]*$'
+kleinbuchstaben_bindestrich_regex = r'^[0-9a-z-_]*$'
 kleinbuchstaben_bindestrich_message = (
-  'Es dürfen nur Kleinbuchstaben – exklusive ä, ö, ü sowie ß – und Bindestriche verwendet werden.'
+  'Es dürfen nur Ziffern, Kleinbuchstaben – exklusive ä, ö, ü sowie ß –, Bindestriche und '
+  'Unterstriche verwendet werden.'
 )
 leerzeichen_bindestrich_regex = r'^(?!.* -).*$'
 leerzeichen_bindestrich_message = 'Im Text darf vor einem Bindestrich kein Leerzeichen stehen.'
