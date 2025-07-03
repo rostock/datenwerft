242106918c9fe00edf58eb2aa20effc9dbe1b505
Sebastian Gutzeit <sebastian.gutzeit@rostock.de>
2025-07-03 15:02:24 +0200

    chore: added initial data to GDI.HRO Metadata app and fixed regex validation

--- toolbox/constants_vars.py
+++ toolbox/constants_vars.py
@@ -49,10 +49,11 @@ d3_message = (
   'Der <strong><em>d.3</em></strong>-Vorgang muss folgendes Format aufweisen '
   '(Beispiele): 512.431-003/002 oder 002.13 oder 114.521-025'
 )
-dateiname_regex = r'^[0-9a-z-_]*(\.?[a-z]*?)$'
+dateiname_regex = r'^[0-9a-z-_]*(\.?[a-z]*?)(\.?[a-z]*?)$'
 dateiname_message = (
   'Es dürfen nur Ziffern, Kleinbuchstaben – exklusive ä, ö, ü sowie ß –, Bindestriche und '
-  'Unterstriche verwendet werden sowie optional am Ende eine durch Punkt abgetrennte Dateiendung.'
+  'Unterstriche verwendet werden sowie optional am Ende eine oder zwei durch Punkt abgetrennte '
+  'Dateiendung(en).'
 )
 doppelleerzeichen_regex = r'^(?!.*  ).*$'
 doppelleerzeichen_message = 'Texte dürfen keine doppelten Leerzeichen enthalten.'
