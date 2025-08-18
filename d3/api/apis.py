c625e9bd5aab332c047307006269820f740893a6
Marcel Borchardt <marcel@brayn.io>
2025-08-18 09:17:15 +0200

    set timeout for login in d3 to 5s

--- d3/api/apis.py
+++ d3/api/apis.py
@@ -39,7 +39,7 @@ class D3AuthenticationApi:
     }
 
     self.logger.debug("Sending request to " + D3_HOST + "/dms/r/ with Headers: " + request_headers.__str__())
-    response = requests.get(D3_HOST + "/dms/r/", headers = request_headers)
+    response = requests.get(D3_HOST + "/dms/r/", headers = request_headers, timeout = 5)
 
     if response.status_code >= 400:
       self.logger.error("Authentication failed with status code " + str(response.status_code) + " and message " + response.text)
