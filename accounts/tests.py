ddca91973930d8bafdf8c4ea9e0350bb81b6f46d
gdmhrogla <stephan.glauner@rostock.de>
2026-04-09 08:48:49 +0200

    Add tests for password change view

--- accounts/tests.py
+++ accounts/tests.py
@@ -85,3 +85,99 @@ class TestLoginForm(AccountTestCase):
     self.assertEqual(response.wsgi_request.user, self.test_user)
     self.test_user.refresh_from_db()
     self.assertIsNotNone(self.test_user.last_login)
+
+
+class TestPasswordChangeView(AccountTestCase):
+  NEW_PASSWORD = 'NewSecret4000!'
+  URL = reverse('accounts:password_change')
+
+  def setUp(self):
+    self.init()
+
+  def test_unauthenticated_get(self):
+    """
+    Nicht-eingeloggte Nutzer erhalten bei GET einen 403-Fehler.
+    """
+    response = self.client.get(self.URL)
+    self.assertEqual(response.status_code, 403)
+
+  def test_unauthenticated_post(self):
+    """
+    Nicht-eingeloggte Nutzer erhalten auch bei POST einen 403-Fehler.
+    """
+    response = self.client.post(self.URL, data={
+      'old_password': self.PASSWORD,
+      'new_password1': self.NEW_PASSWORD,
+      'new_password2': self.NEW_PASSWORD,
+    })
+    self.assertEqual(response.status_code, 403)
+
+  def test_ldap_user_get(self):
+    """
+    LDAP-Nutzer (kein nutzbares Django-Passwort) erhalten bei GET einen 403-Fehler.
+    """
+    self.test_user.set_unusable_password()
+    self.test_user.save()
+    self.client.force_login(self.test_user)
+    response = self.client.get(self.URL)
+    self.assertEqual(response.status_code, 403)
+
+  def test_ldap_user_post(self):
+    """
+    LDAP-Nutzer (kein nutzbares Django-Passwort) erhalten auch bei POST einen 403-Fehler.
+    """
+    self.test_user.set_unusable_password()
+    self.test_user.save()
+    self.client.force_login(self.test_user)
+    response = self.client.post(self.URL, data={
+      'old_password': self.PASSWORD,
+      'new_password1': self.NEW_PASSWORD,
+      'new_password2': self.NEW_PASSWORD,
+    })
+    self.assertEqual(response.status_code, 403)
+
+  def test_local_user_get(self):
+    """
+    Lokale Django-Nutzer können das Passwort-Ändern-Formular aufrufen (200).
+    """
+    self.client.force_login(self.test_user)
+    response = self.client.get(self.URL)
+    self.assertEqual(response.status_code, 200)
+
+  def test_local_user_post_success(self):
+    """
+    Lokale Django-Nutzer werden nach erfolgreichem Passwort-Ändern zur Done-Seite weitergeleitet.
+    """
+    self.client.force_login(self.test_user)
+    response = self.client.post(self.URL, data={
+      'old_password': self.PASSWORD,
+      'new_password1': self.NEW_PASSWORD,
+      'new_password2': self.NEW_PASSWORD,
+    })
+    self.assertRedirects(response, reverse('accounts:password_change_done'))
+
+  def test_local_user_post_wrong_old_password(self):
+    """
+    Ein falsches aktuelles Passwort führt zu einem Formfehler, kein Redirect.
+    """
+    self.client.force_login(self.test_user)
+    response = self.client.post(self.URL, data={
+      'old_password': 'WrongPassword!',
+      'new_password1': self.NEW_PASSWORD,
+      'new_password2': self.NEW_PASSWORD,
+    })
+    self.assertEqual(response.status_code, 200)
+    self.assertTrue(response.context['form'].errors)
+
+  def test_local_user_post_mismatched_passwords(self):
+    """
+    Unterschiedliche neue Passwörter führen zu einem Formfehler, kein Redirect.
+    """
+    self.client.force_login(self.test_user)
+    response = self.client.post(self.URL, data={
+      'old_password': self.PASSWORD,
+      'new_password1': self.NEW_PASSWORD,
+      'new_password2': self.NEW_PASSWORD + 'X',
+    })
+    self.assertEqual(response.status_code, 200)
+    self.assertTrue(response.context['form'].errors)
