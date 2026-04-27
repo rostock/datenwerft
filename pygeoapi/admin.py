a9d36f7d90df6e1154f592f46ed3cbf675aef118
Sebastian Gutzeit <sebastian.gutzeit@rostock.de>
2026-04-27 14:11:10 +0200

    finalized the connection between pygeoapi and GDI.HRO Metadata apps

--- pygeoapi/admin.py
+++ pygeoapi/admin.py
@@ -21,42 +21,48 @@ class CollectionForm(ModelForm):
     label=_('Service-Metadatensatz aus GDI.HRO Metadata'),
   )
 
+  def __init__(self, *args, **kwargs):
+    super().__init__(*args, **kwargs)
+
+    used_ids = set(
+      Collection.objects.exclude(service_id__isnull=True).values_list('service_id', flat=True)
+    )
+
+    qs = Service.objects.filter(type=ServiceType.API_FEATURES)
+
+    # Ensure current instance value is not excluded (edit mode)
+    if self.instance and self.instance.service_id:
+      used_ids.discard(self.instance.service_id)
+
+    # Apply filtering
+    qs = qs.exclude(pk__in=used_ids)
+
+    self.fields['service'].queryset = qs
+
+    # Ensure preselection in edit mode (safe fallback)
+    if self.instance and self.instance.service_id:
+      self.fields['service'].initial = self.instance.service_id
+
   def save(self, commit=True):
     instance = super().save(commit=False)
-    service = self.cleaned_data['service']
-    if service:
-      instance.service_id = service.pk
+    service = self.cleaned_data.get('service')
+    instance.service_id = service.pk if service else None
     if commit:
       instance.save()
     return instance
 
 
 @admin.register(DatabaseConnection)
-class DatabaseConnectionForAdmin(admin.ModelAdmin):
-  list_display = (
-    'id',
-    'host',
-    'dbname',
-    'user',
-  )
-  search_fields = (
-    'host',
-    'dbname',
-    'user',
-  )
+class DatabaseConnectionAdmin(admin.ModelAdmin):
+  ordering = ('host', 'dbname', 'user')
+  list_display = ('id', 'host', 'dbname', 'user')
   empty_value_display = ''
 
 
 @admin.register(Collection)
-class CollectionForAdmin(admin.ModelAdmin):
+class CollectionAdmin(admin.ModelAdmin):
   form = CollectionForm
-  list_display = (
-    'id',
-    'database_connection',
-    'deactivated',
-  )
-  search_fields = ('id',)
-  list_filter = ('database_connection',)
+  list_display = ('id', 'service_display', 'database_connection', 'schema', 'table', 'deactivated')
   fields = (
     'service',
     'database_connection',
@@ -69,6 +75,26 @@ class CollectionForAdmin(admin.ModelAdmin):
     'deactivated',
   )
   empty_value_display = ''
+
+  def get_queryset(self, request):
+    qs = super().get_queryset(request)
+
+    # Collect all service_ids from the queryset
+    service_ids = set(qs.exclude(service_id__isnull=True).values_list('service_id', flat=True))
+
+    # Cache related services in one query (avoid N+1)
+    self._service_cache = {s.id: s for s in Service.objects.filter(id__in=service_ids)}
+
+    return qs
+
+  def service_display(self, obj):
+    service = getattr(self, '_service_cache', {}).get(obj.service_id)
+    if not service:
+      return '-' if not obj.service_id else f'Unknown ({obj.service_id})'
+    return f'({service.name}) {service.title}'
+
+  service_display.short_description = 'Service-Metadatensatz aus GDI.HRO Metadata'
+  service_display.admin_order_field = 'service_id'
 
   def save_model(self, request, obj, form, change):
     super().save_model(request, obj, form, change)
