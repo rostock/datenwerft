ebb203c8877ce7cc02a9deba9f8ea4e6e5fde41d
gdmhrogla <stephan.glauner@rostock.de>
2026-08-25 09:24:48 +0200

    test(accounts): Tests für Passwort-Zurücksetzen

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,6 +1,10 @@
 from django.contrib.auth.models import Group, User
+from django.contrib.auth.tokens import default_token_generator
+from django.core import mail
 from django.test import override_settings
 from django.urls import reverse
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
 from rest_framework.test import APIClient, APITestCase
 
 from angebotsdb.constants_vars import USERS_GROUP
@@ -88,6 +92,138 @@ class TestLoginForm(AccountTestCase):
     self.assertEqual(response.wsgi_request.user, self.test_user)
     self.test_user.refresh_from_db()
     self.assertIsNotNone(self.test_user.last_login)
+
+
+class TestPasswordReset(AccountTestCase):
+  NEW_PASSWORD = 'NewSecret4000!'
+
+  def setUp(self):
+    self.init()
+    self.test_user.email = self.USERNAME
+    self.test_user.save(update_fields=['email'])
+
+  def confirm_url(self):
+    uidb64 = urlsafe_base64_encode(force_bytes(self.test_user.pk))
+    token = default_token_generator.make_token(self.test_user)
+    return reverse(
+      'accounts:password_reset_confirm', kwargs={'uidb64': uidb64, 'token': token}
+    )
+
+  def form_url(self):
+    """
+    Ruft die Confirm-URL ab und liefert die finale Formular-URL.
+    Django maskiert den Token per Redirect auf ``set-password``.
+    """
+    response = self.client.get(self.confirm_url())
+    self.assertEqual(response.status_code, 302)
+    return response.url
+
+  def test_get_form(self):
+    """
+    Die Formular-Seite zeigt das E-Mail-Feld und den statischen LDAP-Hinweis.
+    """
+    response = self.client.get(reverse('accounts:password_reset'))
+    self.assertEqual(response.status_code, 200)
+    self.assertContains(response, 'name="email"')
+    self.assertContains(response, 'zentrale Benutzerkonto')
+
+  def test_post_active_local_user_sends_mail(self):
+    """
+    Ein aktiver lokaler Nutzer erhält genau eine Mail mit Confirm-URL.
+    """
+    response = self.client.post(
+      reverse('accounts:password_reset'), data={'email': self.USERNAME}
+    )
+    self.assertRedirects(response, reverse('accounts:password_reset_done'))
+    self.assertEqual(len(mail.outbox), 1)
+    self.assertEqual(mail.outbox[0].subject, 'Passwort zurücksetzen für Datenwerft.HRO')
+    self.assertEqual(mail.outbox[0].to, [self.USERNAME])
+    self.assertIn(self.confirm_url(), mail.outbox[0].body)
+
+  def test_post_ldap_user_sends_no_mail(self):
+    """
+    LDAP-Nutzer (kein nutzbares Django-Passwort) erhalten keine Mail.
+    """
+    self.test_user.set_unusable_password()
+    self.test_user.save()
+    response = self.client.post(
+      reverse('accounts:password_reset'), data={'email': self.USERNAME}
+    )
+    self.assertRedirects(response, reverse('accounts:password_reset_done'))
+    self.assertEqual(len(mail.outbox), 0)
+
+  def test_post_inactive_user_sends_no_mail(self):
+    """
+    Inaktive Nutzer erhalten keine Mail.
+    """
+    self.test_user.is_active = False
+    self.test_user.save(update_fields=['is_active'])
+    response = self.client.post(
+      reverse('accounts:password_reset'), data={'email': self.USERNAME}
+    )
+    self.assertRedirects(response, reverse('accounts:password_reset_done'))
+    self.assertEqual(len(mail.outbox), 0)
+
+  def test_post_unknown_email_sends_no_mail(self):
+    """
+    Unbekannte E-Mail-Adressen führen zum gleichen Redirect, aber ohne Mail.
+    """
+    response = self.client.post(
+      reverse('accounts:password_reset'), data={'email': 'unknown@example.com'}
+    )
+    self.assertRedirects(response, reverse('accounts:password_reset_done'))
+    self.assertEqual(len(mail.outbox), 0)
+
+  def test_end_to_end_reset(self):
+    """
+    Kompletter Ablauf: Mail anfordern, Confirm-Seite aufrufen,
+    neues Passwort setzen, altes Passwort ist danach ungültig.
+    """
+    self.client.post(reverse('accounts:password_reset'), data={'email': self.USERNAME})
+    self.assertEqual(len(mail.outbox), 1)
+    url = self.form_url()
+    response = self.client.get(url)
+    self.assertEqual(response.status_code, 200)
+    self.assertContains(response, 'Neues Passwort festlegen')
+    response = self.client.post(
+      url,
+      data={
+        'new_password1': self.NEW_PASSWORD,
+        'new_password2': self.NEW_PASSWORD,
+      },
+    )
+    self.assertRedirects(response, reverse('accounts:password_reset_complete'))
+    self.test_user.refresh_from_db()
+    self.assertFalse(self.test_user.check_password(self.PASSWORD))
+    self.assertTrue(self.test_user.check_password(self.NEW_PASSWORD))
+
+  def test_invalid_token_shows_error(self):
+    """
+    Ein ungültiger Token führt zur Fehlermeldung auf der Confirm-Seite.
+    """
+    uidb64 = urlsafe_base64_encode(force_bytes(self.test_user.pk))
+    url = reverse(
+      'accounts:password_reset_confirm',
+      kwargs={'uidb64': uidb64, 'token': 'invalid-token'},
+    )
+    response = self.client.get(url)
+    self.assertEqual(response.status_code, 200)
+    self.assertContains(response, 'ungültig')
+    self.assertNotContains(response, 'name="new_password1"')
+
+  def test_new_password_too_short(self):
+    """
+    Ein zu kurzes neues Passwort verletzt die Validatoren,
+    es tritt ein Formfehler auf und das Passwort bleibt unverändert.
+    """
+    url = self.form_url()
+    response = self.client.post(
+      url, data={'new_password1': 'Kurz123!', 'new_password2': 'Kurz123!'}
+    )
+    self.assertEqual(response.status_code, 200)
+    self.assertTrue(response.context['form'].errors)
+    self.test_user.refresh_from_db()
+    self.assertTrue(self.test_user.check_password(self.PASSWORD))
 
 
 class TestPasswordChangeOnSettingsPage(AccountTestCase):
