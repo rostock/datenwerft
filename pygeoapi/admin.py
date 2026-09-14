aea87f2382d25d6d665532311c8c7caddc803d84
Sebastian Gutzeit <sebastian.gutzeit@rostock.de>
2026-09-14 15:26:37 +0200

    minor linting fix

--- pygeoapi/admin.py
+++ pygeoapi/admin.py
@@ -599,19 +599,14 @@ class CollectionAdmin(admin.ModelAdmin):
     return qs
 
   def get_search_results(self, request, queryset, search_term):
-    queryset, use_distinct = super().get_search_results(
-      request, queryset, search_term
-    )
+    queryset, use_distinct = super().get_search_results(request, queryset, search_term)
 
     if search_term:
       service_ids = Service.objects.filter(
-        Q(name__icontains=search_term) |
-        Q(title__icontains=search_term)
+        Q(name__icontains=search_term) | Q(title__icontains=search_term)
       ).values_list('id', flat=True)
 
-      queryset |= self.model.objects.filter(
-        service_id__in=list(service_ids)
-      )
+      queryset |= self.model.objects.filter(service_id__in=list(service_ids))
 
     return queryset, use_distinct
 
